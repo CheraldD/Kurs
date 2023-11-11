1e16a408ac30c84af4e3b33fa82bc2f7df2a4fd6
Andrew <strigin.andrew@bk.ru>
2023-11-11 10:00:28 +0300

    1

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-
-#include "main.cpp"
-TEST_CASE("Addition", "[add]") {
-    REQUIRE(add(2, 3) == 5);
-    REQUIRE(add(-1, 1) == 0);
-    REQUIRE(add(0, 0) == 0);
-}
