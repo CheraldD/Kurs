6bad2845f375c5d8453c1c4c03cc0527118a0cc1
Andrew <strigin.andrew@bk.ru>
2023-12-25 02:47:12 +0300

    Удаленный старый сервер

--- data_handler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "communicator.h"
-#include <string>
-class data_handler{
-    public:
-    communicator &server;
-    double result;
-    data_handler(int nums_of_vec,communicator &server);
-};
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "ui.h"
-
-int main(int argc, char* argv[])
-{
-    UI interface (argc,argv);
-    return 0;
-}
